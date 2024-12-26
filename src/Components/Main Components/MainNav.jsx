ff76f47b4320397b5bbd35f0ff57e57d8d231453
kasam <kasamsaving@gmail.com>
2024-12-26 12:29:06 +0545

    first

---
+++ src/Components/Main Components/MainNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import BottomNav from './Child Components/BottomNav'
+import TopNav from './Child Components/TopNav'
+
+export default function MainNav() {
+    return (
+        <><TopNav /><BottomNav /></>
+    )
+}
