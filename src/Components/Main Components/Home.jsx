07d186a499a9ab0c09c9618bd34f668566c6a285
Aman Bhandari <bhandariaman311@gmail.com>
2024-12-31 22:41:02 +0545

    Created Landing page

--- src/Components/Main Components/Home.jsx
+++ src/Components/Main Components/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div className='h-[500vh]'>Hooome</div>
+        <div className='flex w-screen px-5 py-8 bg-[rgb(214,214,251)]'>
+            <div className="imageDiv flex-[1]">
+            <img src="/homeImg.png" alt="HomeImage" />
+            </div>
+            <div className="homerightcontent flex-[1] flex flex-col gap-10">
+                <div className="heading text-[rgb(0,130,33)] font-bold text-6xl w-[70%]">Opportunities For ALL</div>
+                <div className="middle text-lg w-[70%]">We are on a mission to ensure equal and accessible opportunities for all students regardless of their location. </div>
+                <button className="explore px-4 py-3 font-bold text-2xl text-white rounded-full bg-[rgb(107,89,172)] w-[260px]">Explore More</button>
+            </div>
+
+        </div>
+
     )
 }
